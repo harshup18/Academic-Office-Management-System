3631e3d3a88baaae1b22f4d990b1ba1b24369263
Aashish Raj Gupta <argupta157@gmail.com>
2018-10-31 17:54:28 +0530

    added README

---
+++ README.md
@@ -0,0 +1,27 @@
+## Academic Office Management System 
+
+### Eclipse EE Dynamic Web Project
+
+Academic Office Management System is a dynamic web application which manages the student course enrollment, student grades and pointer calculation and transcript generation. 
+
+
+
+
+
+### Technologies used
+<ul>
+	<li>Apache Tomcat Server</li>
+	<li>Eclipse EE IDE</li>
+	<li>DB2 Express - 'C'</li>
+</ul>
+
+### App-Developers
+<ul>
+	<li><a href="https://github.com/aashish157">Aashish Raj Gupta</a>
+	</li>
+	<li><a href="https://github.com/Abhinavag1235">Abhinav Agarwal</a>
+	</li>
+	<li><a href="https://github.com/harshup18">Harsh Upadhyay</a>
+	</li>
+	<li><a href="https://github.com/keshavsharma98">Rahul Asnani</a></li>
+</ul>
--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -4,22 +4,23 @@ import javax.servlet.http.*;
 
 public class LoginServlet extends HttpServlet 
 { 
-public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-{
-String user=request.getParameter("username");
-String pass=request.getParameter("password");
-HttpSession session = request.getSession(true); 
-//If correct send to welcome page, else to error page
-if(user.equalsIgnoreCase("admin") && pass.equalsIgnoreCase("admin"))
-{
-session.setAttribute("userName",user);
- response.sendRedirect("/HomePage.jsp");
-}
-else
-{
-session.setAttribute("errorMessage","Invalid Credentials");
- response.sendRedirect("/ErrorPage.jsp");
-}
+	private static final long serialVersionUID = 1L;
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		String user=request.getParameter("username");
+		String pass=request.getParameter("password");
+		HttpSession session = request.getSession(true); 
+	//If correct send to welcome page, else to error page
+	if(user.equalsIgnoreCase("admin") && pass.equalsIgnoreCase("admin"))
+	{
+		session.setAttribute("userName",user);
+		response.sendRedirect("/HomePage.jsp");
+	}
+	else
+	{
+		session.setAttribute("errorMessage","Invalid Credentials");
+		response.sendRedirect("/ErrorPage.jsp");
+	}
  
-}
+	}
 }
